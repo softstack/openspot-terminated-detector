586ec03525894ddd1f8690b5da242f232a4aa278
thiagoRocha <thiago.rocha@to-brasil.com>
2020-08-13 19:14:28 -0300

    finalizando detector

--- functions/api/spot_instance_terminated_resource.py
+++ functions/api/spot_instance_terminated_resource.py
@@ -1,6 +1,31 @@
 import json
+import boto3
+
+def detect(event, context):
+    client = boto3.client('autoscaling')
+    
+    instance = boto3.client('ec2')
+    
+    taginstance = instance.describe_tags(
+        Filters=[
+        {
+            'Name': 'resource-id',
+            'Values': [
+                event["detail"]["instance-id"],
+            ],
+        },
+    ],
+        )
+    
+    print(taginstance['Tags'][0]['Value'])
+    
+    response = client.detach_instances(
+    InstanceIds=[
+        event["detail"]["instance-id"],
+    ],
+    AutoScalingGroupName=taginstance['Tags'][0]['Value'],
+    ShouldDecrementDesiredCapacity=False
+)
 
 
-def handler(event, context):
-    print(json.dumps(event))
-    return event
+    print(response)
