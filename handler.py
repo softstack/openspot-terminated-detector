9c7a67e203edd5273a3d02bab557b35fc6fb46b1
thiagoRocha <thiago.rocha@to-brasil.com>
2020-08-13 15:01:25 -0300

    old

--- handler.py
+++ handler.py
@@ -1,7 +1,13 @@
 import json
 import boto3
 
+cloudwatch = boto3.client('logs')
+
 def detect(event, context):
-    """
     
-    """
+    response = cloudwatch.get_log_events(
+        logGroupName='spot_teste',
+        time=111,
+    )
+
+    print(response)
